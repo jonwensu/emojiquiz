80d7bb2f854099a16951f191bf84ac6905c242f8
jonwensu <wens.sumande@gmail.com>
2022-06-10 16:19:51 +0800

    initial commit

---
+++ src/components/EmojiAnswer.tsx
@@ -0,0 +1,6 @@
+import { PropsWithChildren } from 'types';
+
+const EmojiAnswer: PropsWithChildren = ({ children }) => (
+	<div className='text-6xl text-center text-[#8FBCBB]'>{children}</div>
+);
+export default EmojiAnswer;
---
+++ src/components/EmojiQuestion.tsx
@@ -0,0 +1,6 @@
+import { PropsWithChildren } from 'types';
+
+const EmojiQuestion: PropsWithChildren = ({ children }) => {
+	return <div className='text-9xl'>{children}</div>;
+};
+export default EmojiQuestion;
